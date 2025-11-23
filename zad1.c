a16b44274c21b079459c2d64391b51d4fc43ce18
SkrzekMateusz <mateusz111100@gmail.com>
2025-11-23 14:19:45 +0100

    23_11

--- zad1.c
+++ zad1.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello World!\n");
+    int n;
+    long long suma = 0;
+
+    printf("Podaj liczbę naturalna n: ");
+    scanf("%d", &n);
+
+    if(n<1){
+        printf("n musi byc liczba naturalna >=1\n");
+        return 0;
+    }
+    for(int i = 1; i <= n; i++) {
+        suma += (long long) i*i;
+    }
+
+    printf("Suma kwadratów od 1 do %d wynosi %lld\n, n, suma");
+    return 0;
+
 }
